cd643233e9528f185fadef00c5fa8dd7c9646328
MikheilBregvadze <mikheil.bregvadze@geolab.edu.ge>
2024-01-15 00:19:43 +0400

    static

--- src/services/common.js
+++ src/services/common.js
@@ -9,7 +9,7 @@ export const isLocalhost = Boolean(
 const hostname = process.env.REACT_APP_API_URL || window.location.origin;
 console.log(process.env.REACT_APP_API_URL, window.location.origin);
 export const GetBaseUrl = () => {
-    return isLocalhost ? 'http://localhost:5000' : hostname;
+    return isLocalhost ? 'http://localhost:5000' : 'https://lexiconapi-production.up.railway.app';
 }
 
 export const consoleLog = (msg) => {
